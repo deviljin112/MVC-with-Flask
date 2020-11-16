b38a748f33e11e55d9f6f963628b02438a922100
D3v <deviljin112@users.noreply.github.com>
2020-11-16 11:52:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+# Imports flask into the project
+from flask import Flask, jsonify
 
+# Creates a flask instance
 app = Flask(__name__)
 
 students = [
@@ -12,10 +14,21 @@ students = [
 ]
 
 
+# Used to route traffic to this function
+# "/" means home directory
 @app.route("/")
 def home():
     return "Hello World"
 
 
+# This route will create a new GET route to our link
+# accessible at http://127.0.0.1:5000/api/v1/student/data
+@app.route("/api/v1/student/data", methods=["GET"])
+def student_data():
+    # ETL = Extract Transform Load
+    return jsonify(students)  # Transforms data into JSON
+
+
 if __name__ == "__main__":
+    # Runs the application
     app.run(debug=True)
